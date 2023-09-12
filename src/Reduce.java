ee4bbe74099647a5c24796ebc09d71c09c3e7cc1
Christopher GIANNOULIS <christopher.giannoulis@mail.utoronto.ca>
2023-09-12 14:56:12 -0400

    reducing from 100, number of stepssolution

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Reduce {
-}
+public class Reduce {
+    public static void main(String[] args) {
+        int hund = 100;
+        int count = 0;
+        while (hund != 0) {
+            ;
+            boolean even = hund % 2 == 0;
+            if (even) {
+                hund = hund / 2;
+            }
+            else {
+                hund = hund - 1;
+            }
+            count = count + 1;
+        }
+        System.out.println(count);
+            }
+        }
+
